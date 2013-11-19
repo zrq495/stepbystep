ea4ac2bd925f5ba03752a80c90d450b652200484
zrq495 <zrq495@gmail.com>
2013-11-19 19:46:34 +0800

    add manage and some changes

--- code.py
+++ code.py
@@ -11,21 +11,29 @@ web.config.debug = False
 
 urls =  (
     '/', 'index',
+    '/login', 'login',
     '/admin', 'admin',
+    '/logout', 'logout',
     '/statistics', 'statistics',
     '/statistics/(.+)', 'statistics_year',
+    '/disabled/(.+)/(.+)', 'disabled',
+    '/edituser', 'edituser',
+    '/editdeadline', 'editdeadline',
 )
 
 app = web.application(urls, globals())
 db = web.database(dbn='mysql', db='stepbystep', user='root', pw='rootpass')
 render = web.template.render('templates/', cache=False)
 web.template.Template.globals['render'] = render
-
-curdir = os.path.dirname(__file__)
-session = web.session.Session(app, web.session.DiskStore(curdir + '/' + 'sessions'), initializer={'login': 0, 'privilege':0})
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'login': 0})
 
 cate_len = [13, 20, 17, 14, 9, 12, 7, 7, 19, 17, 5, 14, 26, 15, 7, 24, 10, 15, 10, 4, 4, 9]
 rank = ['初级', '中级', '高级']
+rank_len = [7, 7, 8]
+
+def hashpasswd(passwd):
+    pre = 'sA2lT7!54-'
+    return hashlib.sha1(pre + passwd).hexdigest()
 
 def logged():
     if session.login == 0:
@@ -33,27 +41,12 @@ def logged():
     else:
         return True
 
-def create_render(privilege):
-    if logged():
-        if privilege == 2:
-            render = web.template.render('templates/admin/')
-        else:
-            render = web.template.render('templates/')
-    else:
-        render = web.template.render('templates/')
-    return render
-
-def hashpasswd(passwd):
-    pre = 'sA2lT7!54-'
-    return hashlib.sha1(pre + passwd).hexdigest()
-
 class index:
     def GET(self):
         starttime = time.clock()
-        #user = db.query('select * from user where permission=1 order by grade desc, user_id desc')
         user = db.query(' select u.*, count(s.user_id) as "count" from solution s, user u where s.user_id = u.user_id and u.permission = 1 group by s.user_id order by u.grade desc, u.user_id desc')
         problem = db.query('select * from problem order by pid')
-        solution = db.query('select solution.pid, solution.user_id, solution.actime from solution,user,problem where user.user_id = solution.user_id and problem.pid = solution.pid order by solution.pid, user.grade desc, user.user_id desc')
+        solution = db.query('select solution.pid, solution.user_id, solution.actime from solution,user,problem where user.user_id = solution.user_id and problem.pid = solution.pid and user.permission=1 order by solution.pid, user.grade desc, user.user_id desc')
         user = list(user)
         problem = list(problem)
         solution = list(solution)
@@ -77,12 +70,116 @@ class index:
         print endtime - starttime
         return render.index(pro_list, user, table_width, problem, cate_len)
 
-class admin:
+class login:
     def GET(self):
         if logged():
-            pass
+            raise web.seeother('/admin')
+        return render.login()
+    def POST(self):
+        username = web.input().username
+        passwd = web.input().passwd
+        print 'aaaabbbcccc'
+        #try:
+        ident = db.query('select * from user where user_name = "%s"' %username)[0]
+        print 'aaaa'
+        if ident.poj_name == hashpasswd(passwd):
+            session.login = 1
+            #raise web.seeother('/admin')
+            user = db.query('select * from user where permission != 2 order by grade desc, user_id desc')
+            category = db.query('select problem.cid, category.rank, category.cname, problem.deadline from problem, category where problem.cid = category.cid group by problem.cid order by problem.cid')
+            user = list(user)
+            category = list(category)
+            print 'aaaabbb'
+            return render.admin(user, category, rank_len)
+        else:
+            return render.error('用户名或密码错误！', '/login')
+     #exept:
+        session.login = 0
+        return render.error('error !', '/login')
+
+class admin:
+    def GET(self):
+        print 'bbb'
+        print session.login
+        if logged():
+            user = db.query('select * from user where permission != 2 order by grade desc, user_id desc')
+            category = db.query('select problem.cid, category.rank, category.cname, problem.deadline from problem, category where problem.cid = category.cid group by problem.cid order by problem.cid')
+            user = list(user)
+            category = list(category)
+            return render.admin(user, category, rank_len)
+        else:
+            raise web.seeother('/login')
     def POST(self):
         pass
+
+class disabled:
+    def GET(self, user_id, permission):
+        if not logged():
+            raise web.seeother('/login')
+        try:
+            if permission == '0':
+                db.query('update user set permission=1 where user_id = "%s" and permission=0' %user_id)
+            elif permission == '1':
+                db.query('update user set permission=0 where user_id = "%s" and permission=1' %user_id)
+            else:
+                raise 'error'
+        except:
+            return render.error('disabled or enable error !', '/admin')
+        raise web.seeother('/admin')
+
+class edituser:
+    def GET(self):
+        if logged():
+            raise web.seeother('/admin')
+        else:
+            raise web.seeother('/login')
+    def POST(self):
+        if logged():
+            for key in web.input().keys():
+                value = web.input().get(key)
+                if value:
+                    try:
+                        if key.startswith('editusername'):
+                            l = key.split('-')[1]
+                            db.query('update user set user_name="%s" where user_id="%s"' %(value.encode('utf-8'), l))
+                        elif key.startswith('edituserpojname'):
+                            l = key.split('-')[1]
+                            db.query('update user set poj_name="%s" where user_id="%s"' %(value.encode('utf-8'), l))
+                        elif key.startswith('editusergrade'):
+                            l = key.split('-')[1]
+                            db.query('update user set grade="%s" where user_id="%s"' %(value.encode('utf-8'), l))
+                        elif key.startswith('edituserclass'):
+                            l = key.split('-')[1]
+                            db.query('update user set class1="%s" where user_id="%s"' %(value.encode('utf-8'), l))
+                        else:
+                            return render.error('input error !', '/admin')
+                    except:
+                        return render.error('update user error !', '/admin')
+            raise web.seeother('/admin')
+        else:
+            raise web.seeother('/login')
+
+class editdeadline:
+    def GET(self):
+        raise web.seeother('/login')
+    def POST(self):
+        if logged():
+            for key in web.input().keys():
+                try:
+                    value = web.input().get(key)
+                    da = value.split('-')
+                    if len(da) != 3 or len(da[0]) != 4 or len(da[1]) > 2 or len(da[2]) > 2 or int(da[1]) > 12 or int(da[2] > 31):
+                        return render.error('error !', '/admin')
+                    if value:
+                        l = key.split('-')[1]
+                        db.query('update problem set deadline="%s" where cid = "%s"' %(value.encode('utf-8'), l))
+                    else:
+                        return render.error('error !', '/admin')
+                except:
+                    return render.error('update deadline error !', '/admin')
+            raise web.seeother('/admin')
+        else:
+            raise web.seeother('/login')
 
 class statistics:
     def GET(self):
@@ -90,11 +187,11 @@ class statistics:
         count = []
         for u in user:
             singlecount = []
-            singlecount.append(u.user_name)
+            singlecount.append(u.user_name + ' ' + u.class1)
             for r in rank:
                 sc = db.query('select count(solution.pid) as cnt from solution, problem where solution.user_id="%s" and solution.pid = problem.pid and problem.cid in (select cid from category where rank="%s")' %(u.user_id, r))[0]
                 singlecount.append(int(sc.cnt))
-            singlecount.append(sum(singlecount[1:]))
+            singlecount.append(sum(singlecount[2:]))
             count.append(singlecount)
         return render.statistics(count)
 
@@ -118,12 +215,18 @@ class statistics_year:
                 cnt = [0] * 14
                 data = db.query('select * from solution, user where solution.user_id = user.user_id  and user.user_id = "%s" and solution.actime like "''%s''%%"' %(u.user_id, year))
                 data = list(data)
-                cnt[0] = u.user_name
+                cnt[0] = u.user_name + ' ' + u.class1
                 cnt[13] = len(data)
                 for d in data:
                     cnt[int(d.actime.split('-')[1])] += 1
                 count.append(cnt)
             return render.statistics_year(count, year, start_year, this_year)
+
+class logout:
+    def GET(self):
+        session.login = 0
+        session.kill()
+        raise web.seeother('/')
 
 if __name__ == '__main__':
     app.run()
