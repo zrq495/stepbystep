f553eaf10acd59474226c0d25d8648a633192e6b
zrq495 <zrq495@gmail.com>
2014-01-17 10:27:15 +0800

    添加代理、更改抓取频率

--- crawlpoj.py
+++ crawlpoj.py
@@ -5,12 +5,23 @@ import re
 import urllib2
 import MySQLdb
 import time
+import datetime
 
 conn = MySQLdb.connect('localhost','root','rootpass','stepbystep',charset='utf8')
 cur =conn.cursor()
 
+#proxy = {'http':'27.24.158.155:84'}                                                                                
+proxy = {'http':'127.0.1:8087'}
+proxy_support = urllib2.ProxyHandler(proxy)
+opener = urllib2.build_opener(proxy_support)
+urllib2.install_opener(opener)
+i_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.48'}
+
+today = datetime.date.today()
+
 def get_html(url):
-    res = urllib2.urlopen(url)
+    req = urllib2.Request(url, headers=i_headers) 
+    res = urllib2.urlopen(req)
     html = res.read()
     return html
 
@@ -19,7 +30,7 @@ def re_find(comp, html):
     data = rec.findall(html)
     return data
 
-def crawl_ac(user):
+def crawl_ac(user, t):
     cur.execute('select * from problem')
     problem = cur.fetchall()
     for pro in problem:
@@ -28,14 +39,18 @@ def crawl_ac(user):
         if solution:
             print user[1].encode('utf8'), pro[1], solution[0][2]
         else:
-            statusurl = 'http://poj.org/status?problem_id=%d&user_id=%s&result=0&language=' %(pro[1], user[2]) 
-            html = get_html(statusurl)
-            datas = re_find('<tr align=center><td>.*?</td><td><a href=userstatus\?user_id=.*?</a></td><td><a href=problem\?id=.*?</a></td><td><font color=blue>Accepted</font></td><td>.*?</td><td>.*?</td><td>.*?</td><td>.*?</td><td>(.*?)</td></tr>', html)
-            if datas:
-                actime = datas[-1]
-                print user[1].encode('utf8'), pro[1], actime , 'new'
-                cur.execute('insert into solution(user_id, pid, actime) values(%d, %d, "%s")' %(user[0], pro[0], actime.split()[0]))
-                conn.commit()
+            tt = checkproblem(pro, t)
+            if tt:
+                statusurl = 'http://poj.org/status?problem_id=%d&user_id=%s&result=0&language=' %(pro[1], user[2]) 
+                html = get_html(statusurl)
+                datas = re_find('<tr align=center><td>.*?</td><td><a href=userstatus\?user_id=.*?</a></td><td><a href=problem\?id=.*?</a></td><td><font color=blue>Accepted</font></td><td>.*?</td><td>.*?</td><td>.*?</td><td>.*?</td><td>(.*?)</td></tr>', html)
+                if datas:
+                    actime = datas[-1]
+                    print user[1].encode('utf8'), pro[1], actime , 'new'
+                    cur.execute('insert into solution(user_id, pid, actime) values(%d, %d, "%s")' %(user[0], pro[0], actime.split()[0]))
+                    conn.commit()
+                else:
+                    print user[1].encode('utf8'), pro[1], tt, pro[-1]
                 time.sleep(0.5)
 
 def crawl_solved(user):
@@ -48,11 +63,32 @@ def crawl_solved(user):
         cur.execute('update user set poj_solved=%d, poj_rank=%d where user_id="%s"' %(int(solved[0]), int(rank[0]), user[0]))
         conn.commit()
 
+def checkuser(user):
+    t = (today.year - user[3]) / 4
+    if t < 1:
+        t = 1
+    else:
+        t = today.year - user[3] - 1
+    if today.toordinal() % t == 0:
+        return t
+    return 0
+
+def checkproblem(pro, t):
+    if pro[-1] > 14:
+        t *= 3
+    elif pro[-1] > 7:
+        t *= 2
+    if today.toordinal() % t == 0:                                                               
+        return t
+    else:
+        return 0
 
 if __name__ == '__main__':
     cur.execute('select * from user')
     user = cur.fetchall()
     print time.ctime()
     for u in user:
-        crawl_solved(u)
-        crawl_ac(u)
+        t = checkuser(u)
+        if t:
+            crawl_solved(u)
+            crawl_ac(u, t)
