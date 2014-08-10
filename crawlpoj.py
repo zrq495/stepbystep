04e5c100e875a710fb7b2634e4b2a9ed7c7a12f6
Zhang Ruquan <ruquan.zhang@ramy-inc.com>
2014-08-10 22:37:09 +0800

    添加bestcoder爬虫

--- crawlpoj.py
+++ crawlpoj.py
@@ -12,7 +12,6 @@ cur =conn.cursor()
 temp_conn = MySQLdb.connect('localhost', 'root', '123456', 'oj', charset='utf8')
 temp_cur = temp_conn.cursor()
 
-#proxy = {'http':'27.24.158.155:84'}                                                                                
 #proxy = {'http':'127.0.1:8087'}
 #proxy_support = urllib2.ProxyHandler(proxy)
 #opener = urllib2.build_opener(proxy_support)
@@ -21,16 +20,33 @@ i_headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTM
 
 today = datetime.date.today()
 
+# oj信息，第一个是oj做题数（rating）在数据库中的字段名，第二个是用户在该oj的用户名字段在数据库中的列数， 第三个是oj上做题数有关的页面，第四个是匹配做题数的正则表达式。
+# poj和sdutoj不加，特殊处理
+oj_info = [
+["hdoj_solved", 14, "http://acm.hdu.edu.cn/userstatus.php?user=%s", "<tr><td>Problems Solved</td><td align=center>(\d+)</td></tr>"],
+["cf_rating", 16, "http://codeforces.com/profile/%s", "<span style=\"font-weight:bold;\" class=\"user-.*?\">(\d+)</span> <span"],
+["tc_rating", 18, "http://community.topcoder.com/tc?module=SimpleSearch&ha=%s", "<span class=\"coderTextGray\">(\d+)</span>"],
+["bc_rating", 20, "http://bestcoder.hdu.edu.cn/rating.php?user=%s", "<p>Rating: </p>[\s\S]*?<p>(\d+) \(max \d+\)</p>"],
+]
+
+re_sdutoj = re.compile('<td align="right">Accept[\s\S]*?<td align="left">(\d+)</td>')
+re_poj_ac = re.compile('<tr align=center><td>.*?</td><td><a href=userstatus\?user_id=.*?</a></td><td><a href=problem\?id=.*?</a></td><td><font color=blue>Accepted</font></td><td>.*?</td><td>.*?</td><td>.*?</td><td>.*?</td><td>(.*?)</td></tr>')
+re_poj_solved = re.compile('p\((\d{4})\)')
+re_poj_rank = re.compile('<font color=red>(.*?)</font></td>')
+
+
 def get_html(url):
     req = urllib2.Request(url, headers=i_headers) 
     res = urllib2.urlopen(req, timeout=10)
     html = res.read()
     return html
 
+
 def re_find(comp, html):
     rec = re.compile(r'%s' %(comp))
     data = rec.findall(html)
     return data
+
 
 def poj_ac(user, solved_problem):
     if user[2]:
@@ -46,25 +62,26 @@ def poj_ac(user, solved_problem):
                 if str(pro[1]) in solved_problem:
                     statusurl = 'http://poj.org/status?problem_id=%d&user_id=%s&result=0&language=' %(pro[1], user[2]) 
                     html = get_html(statusurl)
-                    datas = re_find('<tr align=center><td>.*?</td><td><a href=userstatus\?user_id=.*?</a></td><td><a href=problem\?id=.*?</a></td><td><font color=blue>Accepted</font></td><td>.*?</td><td>.*?</td><td>.*?</td><td>.*?</td><td>(.*?)</td></tr>', html)
+                    datas = re_poj_ac(html)
                     if datas:
                         actime = datas[-1]
                         print user[1].encode('utf8'), pro[1], actime , 'new !!!'
                         cur.execute('insert into solution(user_id, pid, actime) values(%d, %d, "%s")' %(user[0], pro[0], actime.split()[0]))
                         conn.commit()
                     else:
-                        #print user[1].encode('utf8'), pro[1], 'waiting' 
+                        #print user[1].encode('utf8'), pro[1], 'waiting'
                         pass
                     time.sleep(0.5)
                 #print user[1].encode('utf8'), pro[1], 'waiting'
+
 
 def poj_solved(user):
     if user[2]:
         userstatusurl = 'http://poj.org/userstatus?user_id=%s' %(user[2])
         html = get_html(userstatusurl)
-        rank = re_find('<font color=red>(.*?)</font></td>', html)
-        solved = re_find('<td align=center width=25%><a href=status\?result=0&user_id=.*?>(.*?)</a></td>', html)
-        problem = re_find('p\((\d{4})\)', html)
+        rank = re_poj_rank.findall(html)
+        solved = re_poj.findall(html)
+        problem = re_poj_solved(html)
         print 'poj', rank, solved
         if rank and solved:
             cur.execute('update user set poj_solved=%d, poj_rank=%d where user_id="%s"' %(int(solved[0]), int(rank[0]), user[0]))
@@ -80,43 +97,21 @@ def sdutoj_solved(user):
             user_id = user_id[0]
             userstatusurl = 'http://acm.sdut.edu.cn/sdutoj/setting.php?userid=%s' %user_id
             html = get_html(userstatusurl)
-            solved = re_find('<td align="right">Accept[\s\S]*?<td align="left">(\d+)</td>', html)
+            solved = re_sdutoj.findall(html)
             print 'sdutoj', solved
             if solved:
                 cur.execute('update user set sdutoj_solved=%d where user_id="%s"' %(int(solved[0]), user[0]))
                 conn.commit()
 
 
-def hdoj_solved(user):
-    if user[14]:
-        userstatusurl = 'http://acm.hdu.edu.cn/userstatus.php?user=%s' %user[14]
+def oj_solved(user, info):
+    if user[info[1]]:
+        userstatusurl = info[2] % user[info[1]]
         html = get_html(userstatusurl)
-        solved = re_find('<tr><td>Problems Solved</td><td align=center>(\d+)</td></tr>', html)
-        print 'hdoj', solved
+        solved = info[3].findall(html)
+        print info[0], solved
         if solved:
-            cur.execute('update user set hdoj_solved=%d where user_id="%s"' %(int(solved[0]), user[0]))
-            conn.commit()
-
-
-def cf_rating(user):
-    if user[16]:
-        userstatusurl = 'http://codeforces.com/profile/%s' %user[16]
-        html = get_html(userstatusurl)
-        solved = re_find('<span style="font-weight:bold;" class="user-.*?">(\d+)</span>', html)
-        print 'cf', solved
-        if solved:
-            cur.execute('update user set cf_rating=%d where user_id="%s"' %(int(solved[0]), user[0]))
-            conn.commit()
-
-
-def tc_rating(user):
-    if user[18]:
-        userstatusurl = 'http://community.topcoder.com/tc?module=SimpleSearch&ha=%s' %user[18].encode('utf-8')
-        html = get_html(userstatusurl)
-        solved = re_find('<span class="coderTextGray">(\d+)</span>', html)
-        print 'tc', solved
-        if solved:
-            cur.execute('update user set tc_rating=%d where user_id="%s"'%(int(solved[0]), user[0]))
+            cur.execute('update user set %s=%d where user_id="%s"' % (info[0], int(solved[0]), user[0]))
             conn.commit()
 
 
@@ -124,12 +119,10 @@ if __name__ == '__main__':
     print time.ctime()
     cur.execute('select * from user')
     user = cur.fetchall()
+    for oj in oj_info:
+        oj[3] = re.compile(r"%s" % oj[3])
     for u in user:
         print u[1].encode('utf-8')
-        solved_problem = poj_solved(u)
-        poj_ac(u, solved_problem)
-        sdutoj_solved(u)
-        hdoj_solved(u)
-        cf_rating(u)
-        tc_rating(u)
+        for oj in oj_info:
+            oj_solved(u, oj)
     print time.ctime()
